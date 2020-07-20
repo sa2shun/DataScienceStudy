d555e631a789f2990b1d8532237bf9c3bfb0ab7e
sasakishun <shunlovessoccer1107@gmail.com>
2020-07-20 20:48:45 +0900

    make it better!

--- main.py
+++ main.py
@@ -8,36 +8,11 @@ num_friends = [100, 49, 41, 40, 25, 21, 21, 19, 19, 18, 18, 16, 15, 15, 15, 15, 
 
 from collections import Counter
 import matplotlib.pyplot as plt
-
 from typing import List
 
-friend_counts = Counter(num_friends)
-xs = range(101)
-ys = [friend_counts[x] for x in xs]
-plt.bar(xs, ys)
-plt.title("Histgram of Friend Counts")
-plt.xlabel("# of friends")
-plt.ylabel("# of people")
-plt.show()
+import enum, ramdom
 
-num_points = len(num_friends)
-largest_value = max(num_friends)
-smallest_value = min(num_friends)
-
-sorted_values = sorted(num_friends)
-smallest_value = sorted_values[0]
-second_smallest_value = sorted_values[1]
-second_largest_value = sorted_values[-2]
-
-def means(xs: List[float]) -> float:
-    return sum(xs) / len(xs)
+class kid(enum.Enum):
 
 
-def mode(x: List[float]) -> List[float]:
-    counts = Counter(x)
-    max_count = max(counts.values())
-    return [x_i for x_i, count in counts.items() if count == max_count]
-
-
-print(mode(num_friends))
 
